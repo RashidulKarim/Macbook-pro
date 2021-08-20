c7048767642df777a2bafed09461c08b89f7c15c
rashidulkarim <rashidul.karim7@gmail.com>
2021-08-21 04:55:59 +0600

    added some comment

--- js/app.js
+++ js/app.js
@@ -1,7 +1,12 @@
+
+// for update innerText of element 
 function setAmount(elementid, amount){
     const elementField = document.getElementById(elementid);
     elementField.innerText = amount;
 }
+
+// for calculation of total cost
+
 function totalAmount(){
     const bestPrice = document.getElementById('bestPrice');
     const bestPriceAmount = parseInt(bestPrice.innerText);
@@ -11,44 +16,44 @@ function totalAmount(){
     const storageCostAmount = parseInt(storageCost.innerText);
     const deliveryCost = document.getElementById('deliveryCost');
     const deliveryCostAmount = parseInt(deliveryCost.innerText);
-    const totalAmount = bestPriceAmount + memoryCostAmount + storageCostAmount + deliveryCostAmount;
     const totalPrice = document.getElementById('totalPrice');
     const totalPriceWithPromo = document.getElementById('totalPriceWithPromo');
+    const totalAmount = bestPriceAmount + memoryCostAmount + storageCostAmount + deliveryCostAmount;
     totalPrice.innerText = totalAmount;
     totalPriceWithPromo.innerText = totalAmount;
 }
 
 // work with memory items
 document.getElementById('8GbMemory').addEventListener('click', function() {
-    setAmount('memoryCost', 0)
-    totalAmount()
+    setAmount('memoryCost', 0);
+    totalAmount();
 })
 document.getElementById('16GbMemory').addEventListener('click', function() {
-    setAmount('memoryCost', 180)
-    totalAmount()
+    setAmount('memoryCost', 180);
+    totalAmount();
 })
 // work with storage items
 document.getElementById('256GbStorage').addEventListener('click', function() {
-    setAmount('storageCost', 0)
-    totalAmount()
+    setAmount('storageCost', 0);
+    totalAmount();
 })
 document.getElementById('512GbStorage').addEventListener('click', function() {
-    setAmount('storageCost', 100)
-    totalAmount()
+    setAmount('storageCost', 100);
+    totalAmount();
 })
 document.getElementById('1TbStorage').addEventListener('click', function() {
-    setAmount('storageCost', 180)
-    totalAmount()
+    setAmount('storageCost', 180);
+    totalAmount();
 })
 
 // work with delivery items
 document.getElementById('freeDelivery').addEventListener('click', function() {
-    setAmount('deliveryCost', 0)
-    totalAmount()
+    setAmount('deliveryCost', 0);
+    totalAmount();
 })
 document.getElementById('chargedDelivery').addEventListener('click', function() {
-    setAmount('deliveryCost', 20)
-    totalAmount()
+    setAmount('deliveryCost', 20);
+    totalAmount();
 })
 
 // work with promo code 
@@ -56,8 +61,9 @@ document.getElementById('promoApplyButton').addEventListener('click', function()
     const promoCode = document.getElementById('promoCode');
     const totalPrice = document.getElementById('totalPriceWithPromo');
     const totalPriceWithoutPromo = document.getElementById('totalPrice');
-    const promoCodeText = promoCode.value;
     const promoMassage = document.getElementById('promoMassage');
+    const applyPromoBtn = document.getElementById('promoApplyButton');
+    const promoCodeText = promoCode.value;
     if(promoCodeText == 'stevekaku'){
         const totalPriceWithoutPromoAmount = parseInt(totalPriceWithoutPromo.innerText)
         const totalPriceWithDiscount = Math.round((totalPriceWithoutPromoAmount/100) * 80);
@@ -65,8 +71,8 @@ document.getElementById('promoApplyButton').addEventListener('click', function()
         promoCode.value = ''
         promoMassage.innerText = 'Congratulation , You got 20% discount.';
         promoMassage.style.color = 'green';
-        document.getElementById('promoApplyButton').setAttribute('disabled', true)
-        document.getElementById('promoApplyButton').backgroundColor = 'gray'
+        applyPromoBtn.setAttribute('disabled', true)
+        applyPromoBtn.style.backgroundColor = 'gray'
     }
     else{
         promoMassage.innerText = "You input an invalid promo code";
